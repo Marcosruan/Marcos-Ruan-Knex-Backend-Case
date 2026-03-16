6c028fbcb7f97be13e564faf7339bede93b88e25
Marcos Ruan <113126971+Marcosruan@users.noreply.github.com>
2026-03-15 23:24:42 -0300

    migração com alteração do orders

---
+++ db/migrations/0004_damp_azazel.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "order_items" ALTER COLUMN "price" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "orders" ALTER COLUMN "total_price" SET DATA TYPE integer;
--- src/db/schema/orders-schema.ts
+++ src/db/schema/orders-schema.ts
@@ -4,21 +4,20 @@ import { users } from './users-schema'
 
 export const orderStatus = pgEnum("order_status", ["PENDING", "PAID", "CANCELLED", "REFUNDED"]);
 
-
 export const orders = pgTable("orders", (t) => ({
 	id: t.uuid("id").primaryKey().notNull().defaultRandom(),
 	user_id: t.uuid("user_id").notNull().references(() => users.id),
 	status: orderStatus("status").notNull().default("PENDING"),
-	total_price: t.numeric("total_price", { precision: 10, scale: 2 }).notNull(),
+	total_price: t.integer("total_price").notNull(),
 	created_at: t.timestamp("created_at").defaultNow().notNull(),
   })
 );
 
 export const order_items = pgTable("order_items", (t) => ({
 	id: t.uuid("id").primaryKey().notNull().defaultRandom(),
-  order_id: t.uuid("order_id").notNull().references(() => orders.id),
-  product_id: t.uuid("product_id").notNull().references(() => products.id),
-	price: t.numeric("price", { precision: 10, scale: 2 }).notNull(),
+  	order_id: t.uuid("order_id").notNull().references(() => orders.id),
+  	product_id: t.uuid("product_id").notNull().references(() => products.id),
+	price: t.integer("price").notNull(),
 	quantity: t.integer("quantity").notNull(),
   })
 );
